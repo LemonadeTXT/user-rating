41f92d5947c2ac4b2cd5e27be344a1460499d0ab
Stecko Anton <lemonadetxt@gmail.com>
2022-11-20 17:05:43 +0300

    added mapping

---
+++ UserRating/UserRating/Mappers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UserRating.Models;
+using UserRating.ViewModels;
+
+namespace UserRating.Infrastructure.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ProfileViewModel, User>().ReverseMap();
+        }
+    }
+}
