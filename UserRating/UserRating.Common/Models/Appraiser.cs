e9e808deaf281ab93d2562eb6e0ed9a7fe3132d7
Stecko Anton <lemonadetxt@gmail.com>
2022-11-20 14:26:34 +0300

    added appraiser class

---
+++ UserRating/UserRating.Common/Models/Appraiser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRating.Common.Models
+{
+    public class Appraiser
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int AppraiserId { get; set; }
+
+        public int Liked { get; set; }
+    }
+}
