086d2db5ae92bf9166dc514f2ab26d4fbbb7bf29
Stecko Anton <lemonadetxt@gmail.com>
2022-12-03 15:39:01 +0300

    removed Error view and class

--- UserRating/UserRating.Common/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UserRating.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
